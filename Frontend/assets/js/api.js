66933eeb7135b30b8b6f31a934d0858f6e1179f7
Mohamed Muzammil <your_email@example.com>
2026-06-15 16:01:55 +0530

    Update CORS for new Netlify URL

--- Frontend/assets/js/api.js
+++ Frontend/assets/js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://securecart-production.up.railway.app";
 
 function getToken() {
     return localStorage.getItem("token");
--- src/main/java/com/securecart/securecart/config/CorsConfig.java
+++ src/main/java/com/securecart/securecart/config/CorsConfig.java
@@ -22,7 +22,8 @@ public class CorsConfig {
                                 "http://127.0.0.1:5500",
                                 "http://localhost:5500",
                                 "http://localhost:63342",
-                                "http://127.0.0.1:63342"// ← ADD THIS
+                                "http://127.0.0.1:63342",
+                                "https://securecart-muzammil.netlify.app"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*")
